62238b5c13779bf66822e572d9d0670ea21b8a55
Abhishek Manu <am88.tech@gmail.com>
2020-05-27 13:21:03 +0530

    Renamed the packages

--- src/main/java/com/assessment/IncubyteAssessmentApplication.java
+++ src/main/java/com/assessment/IncubyteAssessmentApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.assessment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class IncubyteAssessmentApplication {
 
 	public static void main(String[] args) {
